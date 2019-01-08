1b5ec90431facd45ab0bae25bc16655afaf02bac
Wu Haigang <wu.haigang@qq.com>
2019-01-08 20:37:04 +0800

    update

--- springboot-result-demo/src/main/java/com/qikegu/demo/controller/HelloController.java
+++ springboot-result-demo/src/main/java/com/qikegu/demo/controller/HelloController.java
@@ -15,16 +15,21 @@ public class HelloController {
 	@RequestMapping(value="/hello", method = RequestMethod.GET, produces="application/json")
     public ResponseEntity<Result> hello(@RequestParam(value="bad", required=false, defaultValue="false") boolean bad) {
 	    
+		// 结果封装类对象
 		Result res = new Result(200, "ok");
 		
 		if(bad) {
 	    	res.setStatus(400);
 	    	res.setMessage("Bad request");
+	    	
+	    	// ResponseEntity是响应实体泛型，通过它可以设置http响应的状态值，此处返回400
 	        return new ResponseEntity<Result>(res, HttpStatus.BAD_REQUEST);
 		}
         
+		// 把结果数据放进封装类
 		res.putData("words", "Hello world!");
 		
+		// ResponseEntity是响应实体泛型，通过它可以设置http响应的状态值，此处返回200
 		return ResponseEntity.ok(res);
     }
 }
